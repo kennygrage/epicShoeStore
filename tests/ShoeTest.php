df04d7c61a2440e1f16e4a8abb40c6c74efdfecb
Kenny Grage <student@epicodus.com>
2015-08-28 09:40:47 -0700

    Pass 11 shoe tests

--- tests/ShoeTest.php
+++ tests/ShoeTest.php
@@ -196,6 +196,26 @@ class ShoeTest extends PHPUnit_Framework_TestCase {
          $this->assertEquals([$test_shoe2], Shoe::getAll());
      }
 
+     function testAddStore() {
+         //Arrange
+         $store_name = "New Balance";
+         $id = 1;
+         $test_store = new Store($store_name, $id);
+         $test_store->save();
+
+         $shoe_name = "Nike";
+         $id2 = 2;
+         $test_shoe = new Shoe($shoe_name, $id2);
+         $test_shoe->save();
+
+         //Act
+         $test_shoe->addStore($test_store);
+         $result = $test_shoe->getStores();
+
+         //Assert
+         $this->assertEquals([$test_store], $result);
+     }
+
 
 
 
