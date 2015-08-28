d5aff6f076d150024f3a4d1af34e876b740080d8
Kenny Grage <student@epicodus.com>
2015-08-28 08:31:36 -0700

    Pass 1 store test

---
+++ tests/StoreTest.php
@@ -0,0 +1,40 @@
+<?php
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    //Linking class for testing
+    require_once "src/Store.php";
+
+    //Setting server up to apache and mysql passwords.
+    $server = 'mysql:host=localhost;dbname=shoes_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class StoreTest extends PHPUnit_Framework_TestCase {
+
+        //Clears data for next test after each test:
+        protected function tearDown() {
+            Store::deleteAll();
+        }
+
+        //Test getters:
+        function test_getStoreName() {
+
+            //Arrange
+            $store_name = "Portland Running Company";
+            $id = null;
+            $test_store = new Store($store_name, $id);
+            $test_store->save();
+
+            //Act
+            $result = $test_store->getStoreName();
+
+            //Assert
+            $this->assertEquals($result, $store_name);
+        }
+    } //End Class
+?>
